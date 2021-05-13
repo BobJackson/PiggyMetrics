f0c645b3fe838d94067cf755522f71399f0fe49f
Bob Jackson <yousong.wang@highsoft.ltd>
2021-05-13 23:55:47 +0800

    refactor: use Objects.requireNonNull() to wrap

--- notification-service/src/main/java/com/piggymetrics/notification/service/EmailServiceImpl.java
+++ notification-service/src/main/java/com/piggymetrics/notification/service/EmailServiceImpl.java
@@ -4,7 +4,6 @@ import com.piggymetrics.notification.domain.NotificationType;
 import com.piggymetrics.notification.domain.Recipient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ByteArrayResource;
@@ -13,42 +12,45 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import javax.annotation.Resource;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.Objects;
 
 @Service
 @RefreshScope
 public class EmailServiceImpl implements EmailService {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-	@Autowired
-	private JavaMailSender mailSender;
+    @Resource
+    private JavaMailSender mailSender;
 
-	@Autowired
-	private Environment env;
+    @Resource
+    private Environment env;
 
-	@Override
-	public void send(NotificationType type, Recipient recipient, String attachment) throws MessagingException, IOException {
+    @Override
+    public void send(NotificationType type, Recipient recipient, String attachment) throws MessagingException {
 
-		final String subject = env.getProperty(type.getSubject());
-		final String text = MessageFormat.format(env.getProperty(type.getText()), recipient.getAccountName());
+        final String subject = Objects.requireNonNull(env.getProperty(type.getSubject()));
+        final String text = MessageFormat.format(Objects.requireNonNull(env.getProperty(type.getText())),
+                recipient.getAccountName());
 
-		MimeMessage message = mailSender.createMimeMessage();
+        MimeMessage message = mailSender.createMimeMessage();
 
-		MimeMessageHelper helper = new MimeMessageHelper(message, true);
-		helper.setTo(recipient.getEmail());
-		helper.setSubject(subject);
-		helper.setText(text);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(recipient.getEmail());
+        helper.setSubject(subject);
+        helper.setText(text);
 
-		if (StringUtils.hasLength(attachment)) {
-			helper.addAttachment(env.getProperty(type.getAttachment()), new ByteArrayResource(attachment.getBytes()));
-		}
+        if (StringUtils.hasLength(attachment)) {
+            helper.addAttachment(Objects.requireNonNull(env.getProperty(type.getAttachment())),
+                    new ByteArrayResource(attachment.getBytes()));
+        }
 
-		mailSender.send(message);
+        mailSender.send(message);
 
-		log.info("{} email notification has been send to {}", type, recipient.getEmail());
-	}
+        log.info("{} email notification has been send to {}", type, recipient.getEmail());
+    }
 }
